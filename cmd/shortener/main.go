a9bc57cf5e4603a933e3f01ba956baedd63ee29b
zaure.mazhikova <zaure.mazhikova@freedompay.money>
2025-06-07 23:28:43 +0500

    main, tests

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,95 @@
 package main
 
-func main() {}
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"github.com/go-chi/chi/v5"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func generateShortID(n int) (string, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b)[:n], nil
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil || len(body) == 0 {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	originalURL := strings.TrimSpace(string(body))
+	id, err := generateShortID(8)
+	if err != nil || len(body) == 0 {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = os.Setenv(id, originalURL)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	shortURL := fmt.Sprintf("%s%s/%s", flagRunAddr, flagRunPort, id)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(shortURL))
+
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	if id == "" {
+		http.Error(w, "Missing ID", http.StatusBadRequest)
+		return
+	}
+
+	originalURL := os.Getenv(id)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(originalURL))
+
+}
+
+func Router() chi.Router {
+	r := chi.NewRouter()
+
+	r.Post("/", PostHandler)
+	r.Get("/{id}", GetHandler)
+	return r
+}
+
+var flagRunPort string
+var flagRunAddr string
+
+func parseFlags() {
+	flag.StringVar(&flagRunPort, "a", ":8080", "port to run server")
+	flag.StringVar(&flagRunAddr, "b", "http://localhost", "address to run server")
+	flag.Parse()
+}
+
+func main() {
+
+	parseFlags()
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	fmt.Println("Running server on", flagRunPort)
+	return http.ListenAndServe(flagRunPort, Router())
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module github.com/zauremazhikovayandex/url
 
 go 1.21.1
+
+require (
+	github.com/go-chi/chi/v5 v5.2.1
+	github.com/go-resty/resty/v2 v2.16.5
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
