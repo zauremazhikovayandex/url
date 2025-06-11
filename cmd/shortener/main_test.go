4fa88b3f25c1e1e6a241af43223c74126a549856
zaure.mazhikova <zaure.mazhikova@freedompay.money>
2025-06-11 13:01:12 +0500

    2 iter

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestWebhook(t *testing.T) {
+	type TestCase struct {
+		method       string
+		body         string
+		expectedCode int
+		expectedBody string
+	}
+
+	testCases := []TestCase{
+		{
+			method:       http.MethodPost,
+			body:         "https://practicum.yandex.ru/",
+			expectedCode: http.StatusCreated,
+		},
+		{
+			method:       http.MethodPost,
+			body:         "https://yandex.ru/",
+			expectedCode: http.StatusCreated,
+		},
+	}
+
+	for _, tc := range testCases {
+		r := httptest.NewRequest(tc.method, "/", strings.NewReader(tc.body))
+		r.Header.Set("Content-Type", "text/plain")
+		w := httptest.NewRecorder()
+
+		PostHandler(w, r)
+		assert.Equal(t, tc.expectedCode, w.Code, "Код ответа не совпадает с ожидаемым")
+	}
+}
