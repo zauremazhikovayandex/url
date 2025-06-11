ac276c58225678078cdc6c67f7f7898024b1c05f
zaure.mazhikova <zaure.mazhikova@freedompay.money>
2025-06-11 12:30:18 +0500

    1 iter

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,5 +1,88 @@
 package main
 
-func main() {
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
 
+func generateShortID(n int) (string, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b)[:n], nil
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost || r.Header.Get("Content-Type") != "text/plain" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil || len(body) == 0 {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	originalURL := strings.TrimSpace(string(body))
+
+	id, err := generateShortID(8)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = os.Setenv(id, originalURL)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	shortURL := fmt.Sprintf("http://localhost:8080/%s", id)
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte(shortURL))
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	if id == "" {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	originalURL := os.Getenv(id)
+	if originalURL == "" {
+		http.Error(w, "URL not found", http.StatusBadRequest)
+		return
+	}
+
+	http.Redirect(w, r, originalURL, http.StatusTemporaryRedirect)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			PostHandler(w, r)
+		} else if r.Method == http.MethodGet && r.URL.Path != "/" {
+			GetHandler(w, r)
+		} else {
+			http.Error(w, "Bad Request", http.StatusBadRequest)
+		}
+	})
+
+	log.Println("Server started on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
