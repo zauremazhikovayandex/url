2b2416e3fd350327caa796fab72e54463ca0468e
zaure.mazhikova <zaure.mazhikova@freedompay.money>
2025-06-11 19:12:13 +0500

    4 iter

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"flag"
-	"os"
 )
 
 var AppConfig *Config
@@ -13,28 +12,21 @@ type Config struct {
 }
 
 func initURL() (string, string) {
-	serverAddr, baseURL := "", ""
-	defaultServerAddr, defaultBaseURL := ":8080", "http://localhost:8080"
+	serverAddr := ""
+	baseURL := ""
+	defaultServerAddr := ":8080"
+	defaultBaseURL := "http://localhost:8080"
 
 	// флаги
 	flag.StringVar(&serverAddr, "a", "", "port to run server")
 	flag.StringVar(&baseURL, "b", "", "base URL for short links")
 	flag.Parse()
 
-	// ENV переменные
-	envAddr := os.Getenv("SERVER_ADDRESS")
-	envBase := os.Getenv("BASE_URL")
-
-	// приоритет: env > флаг > default
-	if envAddr != "" {
-		serverAddr = envAddr
-	} else if serverAddr == "" {
+	if serverAddr == "" {
 		serverAddr = defaultServerAddr
 	}
 
-	if envBase != "" {
-		baseURL = envBase
-	} else if baseURL == "" {
+	if baseURL == "" {
 		baseURL = defaultBaseURL
 	}
 	return serverAddr, baseURL
