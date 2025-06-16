98c9e348d5390444755c84259a695030c3f2a63b
zaure.mazhikova <zaure.mazhikova@freedompay.money>
2025-06-16 14:28:09 +0500

    2 iter

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,96 +1,21 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
 	"fmt"
 	"github.com/zauremazhikovayandex/url/cmd/config"
-	"io"
-	"log"
+	"github.com/zauremazhikovayandex/url/cmd/routers"
 	"net/http"
-	"os"
-	"strings"
 )
 
-func generateShortID(n int) (string, error) {
-	b := make([]byte, n)
-	_, err := rand.Read(b)
-	if err != nil {
-		return "", err
-	}
-	return base64.URLEncoding.EncodeToString(b)[:n], nil
-}
-
-func PostHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	body, err := io.ReadAll(r.Body)
-	if err != nil || len(body) == 0 {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
-		return
-	}
-
-	originalURL := strings.TrimSpace(string(body))
-	id, err := generateShortID(8)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	err = os.Setenv(id, originalURL)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	shortURL := fmt.Sprintf("%s/%s", config.AppConfig.BaseURL, id)
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusCreated)
-	_, err = w.Write([]byte(shortURL))
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func GetHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	parts := strings.Split(r.URL.Path, "/")
-	if len(parts) < 2 || parts[1] == "" {
-		http.Error(w, "Missing ID", http.StatusBadRequest)
-		return
-	}
-	id := parts[1]
-
-	originalURL := os.Getenv(id)
-	if originalURL == "" {
-		http.Error(w, "URL not found", http.StatusBadRequest)
-		return
-	}
-
-	http.Redirect(w, r, originalURL, http.StatusTemporaryRedirect)
-}
-
 func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
 	config.InitConfig()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/" {
-			PostHandler(w, r)
-		} else {
-			GetHandler(w, r)
-		}
-	})
-
 	fmt.Println("Running server on", config.AppConfig.ServerAddr)
-	returnErr := http.ListenAndServe(config.AppConfig.ServerAddr, nil)
-	if returnErr != nil {
-		log.Fatal(returnErr)
-	}
+	return http.ListenAndServe(config.AppConfig.ServerAddr, routers.Router())
 }
