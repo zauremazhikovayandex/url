93245ba35bb9f8faf4a521f3020b057bb6b3d44c
zaure.mazhikova <zaure.mazhikova@freedompay.money>
2025-07-30 19:57:37 +0500

    15 Инкремент

--- internal/services/url_service_pg.go
+++ internal/services/url_service_pg.go
@@ -3,8 +3,6 @@ package services
 import (
 	"context"
 	"errors"
-	"fmt"
-
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgerrcode"
 	"github.com/zauremazhikovayandex/url/internal/db/postgres"
@@ -29,7 +27,7 @@ func (s *PostgresURLService) SaveURL(ctx context.Context, id string, originalURL
 	if err != nil {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
-			return fmt.Errorf("duplicate_original_url")
+			return postgres.ErrDuplicateOriginalURL
 		}
 		return err
 	}
