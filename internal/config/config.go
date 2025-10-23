a6c281b3e21d259fbf0b86a11e2a6981e26dcd7c
zaure.mazhikova <zaure.mazhikova@freedompay.money>
2025-10-23 17:10:53 +0500

    16 Инкремент

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,10 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sync"
 	"time"
 )
 
-var AppConfig *Config
+var (
+	AppConfig *Config
+	once      sync.Once
+)
 
 type Config struct {
 	ServerAddr     string
@@ -27,71 +31,72 @@ type PostgresConfig struct {
 }
 
 func InitConfig() {
-	// Парсим флаги во временные переменные
-	serverAddrFlag := flag.String("a", "", "port to run server")
-	baseURLFlag := flag.String("b", "", "base URL for short links")
-	fileStorageFlag := flag.String("f", "", "file storage")
-	dbConnectionFlag := flag.String("d", "", "postgres connection")
-	flag.Parse()
+	once.Do(func() {
+		// Парсим флаги во временные переменные
+		serverAddrFlag := flag.String("a", "", "port to run server")
+		baseURLFlag := flag.String("b", "", "base URL for short links")
+		fileStorageFlag := flag.String("f", "", "file storage")
+		dbConnectionFlag := flag.String("d", "", "postgres connection")
+		flag.Parse()
 
-	// Устанавливаем значения по умолчанию
-	serverAddr := ":8080"
-	baseURL := "http://localhost:8080"
-	fileStorage := ""
-	dbConnection := ""
+		// значения по умолчанию
+		serverAddr := ":8080"
+		baseURL := "http://localhost:8080"
+		fileStorage := ""
+		dbConnection := ""
 
-	// Переопределяем флагами
-	if *serverAddrFlag != "" {
-		serverAddr = *serverAddrFlag
-	}
-	if *baseURLFlag != "" {
-		baseURL = *baseURLFlag
-	}
-	if *fileStorageFlag != "" {
-		fileStorage = *fileStorageFlag
-	}
-	if *dbConnectionFlag != "" {
-		dbConnection = *dbConnectionFlag
-	}
+		// флаги (если переданы)
+		if *serverAddrFlag != "" {
+			serverAddr = *serverAddrFlag
+		}
+		if *baseURLFlag != "" {
+			baseURL = *baseURLFlag
+		}
+		if *fileStorageFlag != "" {
+			fileStorage = *fileStorageFlag
+		}
+		if *dbConnectionFlag != "" {
+			dbConnection = *dbConnectionFlag
+		}
 
-	// Окружением (имеет самый высокий приоритет)
-	if env := os.Getenv("SERVER_ADDRESS"); env != "" {
-		serverAddr = env
-	}
-	if env := os.Getenv("BASE_URL"); env != "" {
-		baseURL = env
-	}
-	if env := os.Getenv("FILE_STORAGE_PATH"); env != "" {
-		fileStorage = env
-	}
-	if env := os.Getenv("DATABASE_DSN"); env != "" {
-		dbConnection = env
-	}
+		// окружение
+		if env := os.Getenv("SERVER_ADDRESS"); env != "" {
+			serverAddr = env
+		}
+		if env := os.Getenv("BASE_URL"); env != "" {
+			baseURL = env
+		}
+		if env := os.Getenv("FILE_STORAGE_PATH"); env != "" {
+			fileStorage = env
+		}
+		if env := os.Getenv("DATABASE_DSN"); env != "" {
+			dbConnection = env
+		}
 
-	storageType := "Memory"
-	if dbConnection != "" {
-		storageType = "DB"
-	} else if fileStorage != "" {
-		storageType = "File"
-	}
+		storageType := "Memory"
+		if dbConnection != "" {
+			storageType = "DB"
+		} else if fileStorage != "" {
+			storageType = "File"
+		}
 
-	AppConfig = &Config{
-		ServerAddr:  serverAddr,
-		BaseURL:     baseURL,
-		FileStorage: fileStorage,
-		PGConfig: &PostgresConfig{
-			DBConnection: dbConnection,
-			DBTimeout:    10,
-		},
-		StorageType:   storageType,
-		JWTSecretKey:  "supersecretkey",
-		JWTTokenExp:   time.Hour * 3,
-		JWTCookieName: "auth_token",
-	}
+		AppConfig = &Config{
+			ServerAddr:  serverAddr,
+			BaseURL:     baseURL,
+			FileStorage: fileStorage,
+			PGConfig: &PostgresConfig{
+				DBConnection: dbConnection,
+				DBTimeout:    10,
+			},
+			StorageType:   storageType,
+			JWTSecretKey:  "supersecretkey",
+			JWTTokenExp:   time.Hour * 3,
+			JWTCookieName: "auth_token",
+		}
 
-	fmt.Println("Storage type:", storageType)
-
-	if storageType == "File" {
-		fmt.Println("💾 Using file storage path:", AppConfig.FileStorage) // отладка
-	}
+		fmt.Println("Storage type:", storageType)
+		if storageType == "File" {
+			fmt.Println("💾 Using file storage path:", AppConfig.FileStorage)
+		}
+	})
 }
